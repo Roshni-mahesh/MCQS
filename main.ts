25a2facb03884a74cc02f5d0410e1ffbcfc6b7b2
Roshni Mahesh <160133342+Roshni-mahesh@users.noreply.github.com>
2024-04-02 08:26:21 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -34,3 +34,57 @@ if (answer.userGuessedAnswer2 === questionAnswer2){
 }else{
     console.log("you guessed wrong answer.")
 };
+
+
+const questionAnswer3 = "Application Programming Interface";
+
+const answer3 = await inquirer.prompt([
+    {
+    name:"MCQS",
+    message: "select the correct full-form of (API)",
+    type:"number",
+    chocies:"(A)Artificial Programming Intelligence.),(B)Application Programming Interface)"
+    }
+]);
+
+if (answer.userGuessedAnswer3 === questionAnswer3){
+    console.log("congratulations! you guessed right answer.")
+}else{
+    console.log("you guessed wrong answer.")
+};
+
+
+const questionAnswer4 = "Amazon Web Services";
+
+const answer4 = await inquirer.prompt([
+    {
+    name:"MCQS",
+    message: "select the correct full-form of (AWS)",
+    type:"number",
+    chocies:"(A)American Welding Society),(B)Amazon Web Services)"
+    }
+]);
+
+if (answer.userGuessedAnswer4 === questionAnswer4){
+    console.log("congratulations! you guessed right answer.")
+}else{
+    console.log("you guessed wrong answer.")
+};
+
+
+const questionAnswer5 = "Blind Carbon Copy";
+
+const answer5 = await inquirer.prompt([
+    {
+    name:"MCQS",
+    message: "select the correct full-form of (BCC)",
+    type:"number",
+    chocies:"(A)Blind Carbon Copy),(B) body-centred cubic)"
+    }
+]);
+
+if (answer.userGuessedAnswer5 === questionAnswer5){
+    console.log("congratulations! you guessed right answer.")
+}else{
+    console.log("you guessed wrong answer.")
+};
